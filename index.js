4bdfa65d1cc0c9b8eb4553f296a9cacb4cd44f30
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-01-22 09:52:44 -0500

    adfghjkl

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const DOMSelectors = {
     form: document.querySelector("form"),
 };
 
-function inserting(){
-    let buttons = document.querySelectorAll(".values") 
-    buttons.forEach((btn) => btn.addEventListener("click", function(event){
-         event.preventDefault();
-         let numValues = buttons.textContent
-         console.log(numValues)
-        //  DOMSelectors.form.insertAdjacentHTML("afterbegin", numValues)
-        })); 
-    };
-    inserting() 
+// function inserting(){
+//     let buttons = document.querySelectorAll(".values") 
+//     buttons.forEach((btn) => btn.addEventListener("click", function(event){
+//          event.preventDefault();
+//          let numValues = buttons.textContent
+//          console.log(numValues)
+//         //  DOMSelectors.form.insertAdjacentHTML("afterbegin", numValues)
+//         })); 
+//     };
+//     inserting() 
+function clearFields(){
+    inputNums1.value = "";
+    inputNums2.value = "";
+}
+
+let x = document.getElementById("inputNums1.value")
+let y = document.getElementById("inputNums2.value")
+DOMSelectors.form.addEventListener("submit", function(event){
+    event.preventDefault();
+    clearFields();
+    console.log(x, y)
+});
