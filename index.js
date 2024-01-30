767e2c11cabb87aabc2fefe70636bfeda28da04f
KatieT12 <144460461+KatieT12@users.noreply.github.com>
2024-01-30 10:28:03 -0500

    aaaaa

--- index.js
+++ index.js
@@ -2,27 +2,52 @@ const DOMSelectors = {
     submitbtn: document.getElementById("submit"),
     inputTitle: document.getElementById('inputTitle'),
     form: document.querySelector("form"),
+    x: document.getElementById("inputNums1"),
+    y: document.getElementById("inputNums2"),
+    add: document.getElementById("add"),
+    subtract: document.getElementById("subtract"),
+    multiply: document.getElementById("multiply"),
+    divide: document.getElementById("divide"),
+    exponentation: document.getElementById("exponentation"),
 };
-
-// function inserting(){
-//     let buttons = document.querySelectorAll(".values") 
-//     buttons.forEach((btn) => btn.addEventListener("click", function(event){
-//          event.preventDefault();
-//          let numValues = buttons.textContent
-//          console.log(numValues)
-//         //  DOMSelectors.form.insertAdjacentHTML("afterbegin", numValues)
-//         })); 
-//     };
-//     inserting() 
+ 
 function clearFields(){
-    inputNums1.value = "";
-    inputNums2.value = "";
+    DOMSelectors.x.value = "";
+    DOMSelectors.y.value = "";
 }
 
-let x = document.getElementById("inputNums1.value")
-let y = document.getElementById("inputNums2.value")
-DOMSelectors.form.addEventListener("submit", function(event){
+//add
+DOMSelectors.add.addEventListener("click", function(event){
     event.preventDefault();
+    // console.log(DOMSelectors.x.value + DOMSelectors.y.value)
+    let sum = DOMSelectors.x.value + DOMSelectors.y.value
+    console.log(sum)
     clearFields();
-    console.log(x, y)
 });
+//subtract
+DOMSelectors.subtract.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.x.value + DOMSelectors.y.value)
+    clearFields();
+});
+//multiply
+DOMSelectors.multiply.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.x.value + DOMSelectors.y.value)
+    clearFields();
+});
+//divide
+DOMSelectors.divide.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.x.value + DOMSelectors.y.value)
+    clearFields();
+});
+//exponent
+DOMSelectors.exponentation.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log(DOMSelectors.x.value + DOMSelectors.y.value)
+    clearFields();
+});
+
+
+
